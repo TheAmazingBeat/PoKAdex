ac9950c728851f9a7b04c46dca0dd0cf9410e537
TheAmazingBeat <ice.baldivas@gmail.com>
2022-05-12 23:44:48 -0500

    Save Files

--- routes/pokedex.js
+++ routes/pokedex.js
@@ -3,21 +3,43 @@ const axios = require('axios');
 const express = require('express');
 const app = express.Router();
 
-async function getPokemon() {
+async function getPokedexData() {
   try {
-    return await axios.get('https://pokeapi.co/api/v2/pokemon/?limit=20');
+    return await axios.get('https://pokeapi.co/api/v2/pokemon/?limit=151');
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getPokemon(pokeUrl) {
+  try {
+    const pokeDetails = await axios.get(pokeUrl);
+    const pokemon = {
+      name: pokeDetails.name
+    }
+    return pokemon;
   } catch (error) {
     console.log(error);
   }
 }
 
 app.get('/', async (req, res) => {
-  const pokeData = await getPokemon();
+  const pokeData = await getPokedexData();
+  const results = pokeData.data.results;
+  let pokeDex = [];
 
-  if (pokeData.status == 200) console.log(pokeData.data);
+  if (pokeData.status == 200) {
+    pokeDex.push(await Promise.all(
+      results.map(async (el) => {
+        const pokemonData = await getPokemon(el.url);
+      })
+    ));
+    // });
+    console.log(pokeDex);
+  }
 
   res.render('index', {
-    data: pokeData.data.results,
+    dex: pokeDex,
   });
 });
 
