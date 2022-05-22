e2477186424f8ab33ef6055d079187813721c078
TheAmazingBeat <ice.baldivas@gmail.com>
2022-05-22 01:39:01 -0500

    Pokemon Details and Pagination Prototype

--- public/scripts/pokedex.js
+++ public/scripts/pokedex.js
@@ -1,8 +1,17 @@
 const app = document.querySelector('#app');
 const API_ROOT = `https://pokeapi.co/api/v2/`;
-let pd = [];
+const ITEMS_PER_PAGE = 50;
+let OFFSET = 0;
+let pokedex = [];
 
-window.onload = displayPokedex();
+window.onload = displayPokedex(OFFSET, ITEMS_PER_PAGE);
+window.onclick = (e) => {
+  // console.log(e.target)
+  const modal = document.querySelector('.poke-modal-overlay');
+  if (e.target == modal) {
+    modal.classList.toggle('show');
+  }
+};
 
 async function getPokedexData(gameURL) {
   try {
@@ -47,9 +56,14 @@ async function getPokemon(pokeURL) {
   }
 }
 
-function createPokemonCard(pokemon) {
+function createPokemonCard(pokemon, index) {
   const pokeContainer = document.createElement('a');
   pokeContainer.classList.add('poke-container');
+  pokeContainer.dataset.index = index;
+  pokeContainer.onclick = () => {
+    displayPokemonModal(index);
+  };
+  // pokeContainer.onclick = displayPokemonModal(index);
 
   const id = document.createElement('p');
   id.classList.add('pokemon-id');
@@ -80,19 +94,77 @@ function createPokemonCard(pokemon) {
   return pokeContainer;
 }
 
-async function displayPokedex() {
-  const pokedex = await getPokedexData();
-  console.table(pokedex);
-  const dexContainer = document.createElement('div');
-  dexContainer.setAttribute('id', 'dex');
-  pokedex.forEach((pokemon) => {
-    dexContainer.append(createPokemonCard(pokemon));
-  });
+async function displayPokedex(offset, limit) {
+  if (pokedex.length <= 0) {
+    const pokedexData = await getPokedexData();
+    pokedex = pokedexData;
+  }
+
+  let dexContainer;
+
+  if (dexContainer == undefined) {
+    dexContainer = document.createElement('div');
+    dexContainer.setAttribute('id', 'dex');
+  }
+
+  // Hides the spinner once pokedex is loaded, but cards hasn't rendered
+  const spinner = document.querySelector('.spinner');
+  spinner.classList.toggle('hide');
+
+  for (let i = offset; i < limit + offset; i++) {
+    dexContainer.append(createPokemonCard(pokedex[i], i));
+  }
+
+  function createPagination() {
+    const totalPages = Math.ceil(pokedex.length / ITEMS_PER_PAGE);
+    const pageButtons = document.createElement('div');
+    pageButtons.classList.add('pagination-buttons');
+    for (let b = 0; b < totalPages; b++) {
+      const pageNum = b + 1;
+      const button = document.createElement('button');
+      button.classList.add('page-button');
+      button.innerText = pageNum;
+      button.onclick = () => {
+        console.log('page clicked');
+        // Empty the pokedex
+        dexContainer.replaceChildren();
+        // Shows spinner
+        spinner.classList.toggle('hide');
+        displayPokedex(OFFSET + (limit * pageNum - 1), ITEMS_PER_PAGE);
+      };
+      pageButtons.append(button);
+    }
+    app.append(pageButtons);
+  }
+
+  if (document.querySelector('.pagination-buttons') == null) createPagination();
 
   app.append(dexContainer);
 
-  pd = pokedex;
   return pokedex;
 }
 
-app.innerHTML = `<h1>PoKAdex</h1>`;
+async function displayPokemonModal(pokeIndex) {
+  const pokemon = pokedex[pokeIndex];
+  const modal = document.querySelector('.poke-modal-overlay');
+  modal.classList.toggle('show');
+  const modalHeader = document.querySelector('.modal-header');
+  const modalBody = document.querySelector('.modal-body');
+  const modalooter = document.querySelector('.modal-footer');
+
+  modalHeader.innerHTML = `<h2 class="pokemon-name">#${pokemon.id} - ${pokemon.name}</h2>`;
+  modalBody.innerHTML = `<img src="${pokemon.image}" alt="Picture of ${pokemon.name}" class="pokemon-image" loading="lazy">`;
+}
+
+app.innerHTML = `
+<h1>PoKAdex</h1>
+<div class="spinner"></div>
+<div class="poke-modal-overlay">
+  <div class="poke-modal">
+    <header class="modal-header"></header>
+    <main class="modal-body"></main>
+    <footer class="modal-footer"></footer>
+  </div>
+</div>
+      
+`;
